5bacf73c5866d371bc2fc127cf1e5b1c7c282232
Tahmina <tahminakhatun5447@gmail.com>
2023-01-18 17:27:42 +0600

    new setting

--- index.js
+++ index.js
@@ -1,24 +1,30 @@
-const express = require("express");
-const cors = require("cors");
-const { MongoClient, ServerApiVersion } = require('mongodb');
-require("dotenv").config();
-
+const express = require('express');
 const app = express();
+const cors = require('cors');
 const port = process.env.PORT || 5000;
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+
+require('dotenv').config();
 
 app.use(cors());
 app.use(express.json());
 
 
-const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.8uhbkvb.mongodb.net/?retryWrites=true&w=majority`;
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.01makvu.mongodb.net/?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
 
+// eee
+async function run(){
+  try{
+    const usersCollection = client.db('fitLessian').collection('users');
+    const user={name:"tahmina",roll:"developer"}
+    const result= await usersCollection.insertOne(user);
+    console.log(result);
+  }finally{
 
-app.get('/', (req, res) => {
-  res.send('Welcome to Fitlessian family!')
-})
-
-app.listen(port, () => {
-  console.log(`Welcome to fitlessian ${port}`)
-})
+  }
+}
+run().catch(err => console.log(err));
