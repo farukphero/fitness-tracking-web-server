b2d1df63f12bac7630fbaabd4377cc3bce420982
sadekinchowdhury <rumelchow340@gmail.com>
2023-01-19 18:24:36 +0600

    users

--- index.js
+++ index.js
@@ -11,20 +11,37 @@ app.use(express.json());
 
 
 
-
-const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.01makvu.mongodb.net/?retryWrites=true&w=majority`;
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@clusterfit.lgaupy2.mongodb.net/?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
-
 // eee
-async function run(){
-  try{
-    const usersCollection = client.db('fitLessian').collection('users');
-    const user={name:"tahmina",roll:"developer"}
-    const result= await usersCollection.insertOne(user);
-    console.log(result);
-  }finally{
+async function run() {
+  try {
+    const UsersCollection = client.db("fitlessian").collection("User")
+
+
+    app.post('/users', async (req, res) => {
+      const user = req.body
+      const result = await UsersCollection.insertOne(user)
+      res.send(result)
+    })
+
+    app.get('/user/:id', async (req, res) => {
+      const id = req.params.id
+      const query = { _id: ObjectId(id) }
+      const result = await UsersCollection.findOne(query)
+      res.send(result)
+    })
+
+
+  } finally {
 
   }
 }
-run().catch(err => console.log(err));
+run()
+app.get('/', (req, res) => {
+  res.send('Start fitlessian')
+})
+app.listen(port, () => {
+  console.log(`this server is running on ${port}`)
+})
