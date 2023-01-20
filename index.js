3df34636128be38f95f6bd574edd8e991d85f3c3
farukphero <farukk6319@gmail.com>
2023-01-20 16:02:26 +0600

    Service added

--- index.js
+++ index.js
@@ -19,61 +19,35 @@ const client = new MongoClient(uri, {
 // eee
 async function run() {
   try {
- 
-    const UsersCollection = client.db("fitlessian").collection("User");
 
-    app.post("/users", async (req, res) => {
-      const user = req.body;
-      const result = await UsersCollection.insertOne(user);
-      res.send(result);
-    });
-
-    app.get("/users/info", async (req, res) => {
-      const email = req.query.email;
-      const result = await UsersCollection.findOne({ email: email });
-      res.send(result);
-    });
-
-    // not a proper api
-    // app.get("/user/:id", async (req, res) => {
-    //   const id = req.params.id;
-    //   const query = { _id: ObjectId(id) };
-    //   const result = await UsersCollection.findOne(query);
-    //   console.log(result);
-    //   res.send(result);
-    // });
-
-    app.get("/test", async (req, res) => {
-      const result = await UsersCollection.find({}).toArray();
-      res.send(result);
-    });
- 
     const UsersCollection = client.db("fitlessian").collection("User")
     const servicesCollection = client.db("fitlessian").collection("services")
 
-
-    app.post('/users', async (req, res) => {
-      const user = req.body
-      const result = await UsersCollection.insertOne(user)
-      res.send(result)
-    })
-
-    app.get('/user/:email', async (req, res) => {
+    app.get('/users/:email', async (req, res) => {
       const email = req.params.email;
       const query = { email : email }
       const result = await UsersCollection.findOne(query)
       res.send(result)
     })
    
+    app.post('/users', async (req, res) => {
+      const user = req.body
+      const result = await UsersCollection.insertOne(user)
+      res.send(result)
+    })
+
 
     app.get('/services', async (req, res) => {
       const query = {};
       const services = await servicesCollection.find(query).toArray();
       res.send(services)
     })
-
-
- 
+    app.get('/users', async (req, res) => {
+      const query = {};
+      const services = await UsersCollection.find(query).toArray();
+      res.send(services)
+    })
+    
   } finally {
   }
 }
