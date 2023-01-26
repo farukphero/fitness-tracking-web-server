d24fd9c407bdad3cc786c5e00f5bd605e24f8714
farukphero <farukk6319@gmail.com>
2023-01-26 23:39:52 +0600

    Admin route create

--- index.js
+++ index.js
@@ -16,110 +16,109 @@ const client = new MongoClient(uri, {
   serverApi: ServerApiVersion.v1,
 });
 
-
 // eee
 async function run() {
   try {
-    const UsersCollection = client.db("fitlessian").collection("User");
+    const usersCollection = client.db("fitlessian").collection("User");
     const servicesCollection = client.db("fitlessian").collection("services");
     const FoodsCollection = client.db(`fitlessian`).collection(`foods`);
-    const ActivitiesCollection = client.db(`fitlessian`).collection(`Activities`);
-    const foodCollection = client.db("fitlessian").collection("foods")
-    const loggedFoodCollection = client.db("fitlessian").collection("loggedFood")
-    const tutorialCollection = client.db("fitlessian").collection("tutorials")
-    const categoryCollection = client.db("fitlessian").collection("category")
-    const favoriteFoodCollection = client.db("fitlessian").collection("favouriteFood")
+    const ActivitiesCollection = client
+      .db(`fitlessian`)
+      .collection(`Activities`);
+    const foodCollection = client.db("fitlessian").collection("foods");
+    const loggedFoodCollection = client
+      .db("fitlessian")
+      .collection("loggedFood");
+    const tutorialCollection = client.db("fitlessian").collection("tutorials");
+    const categoryCollection = client.db("fitlessian").collection("category");
+    const favoriteFoodCollection = client
+      .db("fitlessian")
+      .collection("favouriteFood");
 
-    app.post('/tutorial', async (req, res) => {
+    app.post("/tutorial", async (req, res) => {
       const post = req.body;
       const result = await tutorialCollection.insertOne(post);
-      console.log(result)
+      console.log(result);
       res.send(result);
-    })
+    });
 
-    app.get('/tutorials',async(req,res)=>{
-        
-      const category=req.query.category;
-      const query={
-        category:category
-      }
-       const cursor=tutorialCollection.find(query);
-       const result=await cursor.toArray();
-       res.send(result);
-      
-     })
+    app.get("/tutorials", async (req, res) => {
+      const category = req.query.category;
+      const query = {
+        category: category,
+      };
+      const cursor = tutorialCollection.find(query);
+      const result = await cursor.toArray();
+      res.send(result);
+    });
 
-     app.post('/category', async (req, res) => {
+    app.post("/category", async (req, res) => {
       const post = req.body;
       const result = await categoryCollection.insertOne(post);
-      console.log(result)
+      console.log(result);
       res.send(result);
-    })
-    
-    app.get('/singleCategory/:id',async(req,res)=>{
-      const id=req.params.id;
-      const query={_id:ObjectId(id)}
-      const result=await categoryCollection.findOne(query);
-      res.send(result);
-    })
+    });
 
-    app.get('/categories',async(req,res)=>{
-      const query={};
-      const categories=await categoryCollection.find(query).toArray();
+    app.get("/singleCategory/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: ObjectId(id) };
+      const result = await categoryCollection.findOne(query);
+      res.send(result);
+    });
+
+    app.get("/categories", async (req, res) => {
+      const query = {};
+      const categories = await categoryCollection.find(query).toArray();
       res.send(categories);
-    })
+    });
     // --------------
-    app.get('/users/:email', async (req, res) => {
-      const email = req.params.email;
-      const query = { email : email }
-      const result = await servicesCollection.findOne(query)
- 
-      res.send(result)
-    })
-
-    app.post('/users', async (req, res) => {
-      const user = req.body
-      const result = await UsersCollection.insertOne(user)
-      res.send(result)
-    })
-
-
-    app.get('/services', async (req, res) => {
- 
     app.get("/users/:email", async (req, res) => {
       const email = req.params.email;
+      console.log(email)
       const query = { email: email };
-      const result = await UsersCollection.findOne(query);
+      const result = await servicesCollection.findOne(query);
+
       res.send(result);
     });
 
     app.post("/users", async (req, res) => {
       const user = req.body;
-      const result = await UsersCollection.insertOne(user);
+      const result = await usersCollection.insertOne(user);
       res.send(result);
     });
 
     app.get("/services", async (req, res) => {
- 
-      const query = {};
-      const services = await servicesCollection.find(query).toArray();
-      res.send(services);
-    });
-    app.get("/users", async (req, res) => {
-      const query = {};
-      const services = await UsersCollection.find(query).toArray();
- 
-      res.send(services)
-    })
- 
+      app.get("/users/:email", async (req, res) => {
+        const email = req.params.email;
+        const query = { email: email };
+        const result = await usersCollection.findOne(query);
+        res.send(result);
+      });
 
- 
-    app.get('/foods', async (req, res) => {
-      const query = {};
-      const foods = await foodCollection.find(query).toArray();
-      res.send(foods)
-    })
- 
+      app.post("/users", async (req, res) => {
+        const user = req.body;
+        const result = await usersCollection.insertOne(user);
+        res.send(result);
+      });
+
+      app.get("/services", async (req, res) => {
+        const query = {};
+        const services = await servicesCollection.find(query).toArray();
+        res.send(services);
+      });
+      app.get("/users", async (req, res) => {
+        const query = {};
+        const services = await usersCollection.find(query).toArray();
+
+        res.send(services);
+      });
+
+      app.get("/foods", async (req, res) => {
+        const query = {};
+        const foods = await foodCollection.find(query).toArray();
+        res.send(foods);
+      });
+
       res.send(services);
     });
 
@@ -143,39 +142,60 @@ async function run() {
 
     app.get(`/activities`, async (req, res) => {
       const email = req.query.activist;
-      const query = { activist: email };      
+      const query = { activist: email };
       const result = await ActivitiesCollection.find(query).toArray();
       res.send(result);
     });
- 
-    
-    app.post('/loggedFood', async (req, res) => {
+
+    app.post("/loggedFood", async (req, res) => {
       const loggedFood = req.body;
       const result = await loggedFoodCollection.insertOne(loggedFood);
       res.send(result);
-  })
+    });
 
-  app.get('/favouriteFood/:email', async (req,res)=>{
-    const email = req.params.email;
-    const query = { userEmail : email};
-    const favoriteFood = await favoriteFoodCollection.find(query).toArray();
-    res.send(favoriteFood)
-  })
- 
-// favourite Food
-    app.post('/favouriteFood', async (req, res) => {
+    app.get("/favouriteFood/:email", async (req, res) => {
+      const email = req.params.email;
+      const query = { userEmail: email };
+      const favoriteFood = await favoriteFoodCollection.find(query).toArray();
+      res.send(favoriteFood);
+    });
+
+    // favourite Food
+    app.post("/favouriteFood", async (req, res) => {
       const favoriteFood = req.body;
       const result = await favoriteFoodCollection.insertOne(favoriteFood);
       res.send(result);
-  })
- 
-  app.get('/loggedFood/:email', async (req,res)=>{
-    const email = req.params.email;
-    const query = { userEmail : email};
-    const loggedFood = await loggedFoodCollection.find(query).toArray();
-    res.send(loggedFood)
-  })
- 
+    });
+
+    app.get("/loggedFood/:email", async (req, res) => {
+      const email = req.params.email;
+      const query = { userEmail: email };
+      const loggedFood = await loggedFoodCollection.find(query).toArray();
+      res.send(loggedFood);
+    });
+    app.get('/users/admin/:email', async (req, res)=>{
+      const email = req.params.email;
+      const query = {email: email}
+      const user = await usersCollection.findOne(query);
+      res.send({isAdmin: user?.role === 'admin'})
+    })
+
+    app.put("/users/admin/:id", async (req, res) => {
+      const id = req.params.id;
+      const filter = { _id: ObjectId(id) };
+      const options = { upsert: true };
+      const updatedDoc = {
+        $set: {
+          role: "admin",
+        },
+      };
+      const result = await usersCollection.updateOne(
+        filter,
+        updatedDoc,
+        options
+      );
+      res.send(result);
+    });
   } finally {
   }
 }
